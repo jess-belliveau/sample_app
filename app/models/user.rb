9baab6ba826a8ce9e107dfa87e98f7cb45b71e3a
Jess Belliveau <jess.belliveau@gmail.com>
2012-07-31 15:29:02 +1000

    Completed exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 	attr_accessible :email, :name, :password, :password_confirmation
 	has_secure_password
 
-	before_save { |user| user.email = email.downcase }
+	before_save { self.email.downcase! }
 
 	validates :password, :presence => true, :length => { :minimum => 6 }
 	validates :password_confirmation, :presence => true
