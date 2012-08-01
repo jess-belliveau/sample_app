5dedeeefd53d701ef177cb06370f7f7be5fd7cb3
Jess Belliveau <jess.belliveau@gmail.com>
2012-08-01 15:24:26 +1000

    Finished sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,17 @@ class User < ActiveRecord::Base
 
 	before_save { self.email.downcase! }
 
+	before_save :create_remember_token
+
 	validates :password, :length => { :minimum => 6 }
 	validates :password_confirmation, :presence => true
 	validates :name, :presence => true, :length => { :maximum => 50 }
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => { :case_sensitive => false }
+
+	private
+	def create_remember_token
+		self.remember_token = SecureRandom.hex
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 SampleApp::Application.routes.draw do
 	root :to => 'static_pages#home'
 
+#	Users resources - allow all methods
 	resources :users
+	match '/signup',  :to => 'users#new'
 
+#	Standard routes for static_pages
 	match '/help',    :to => 'static_pages#help'
 	match '/about',   :to => 'static_pages#about'
 	match '/contact', :to => 'static_pages#contact'
-	match '/signup',  :to => 'users#new'
 
-#	get "static_pages/home"
-#	get "static_pages/help"
-#	get "static_pages/about"
-#	get "static_pages/contact"
+#	Sessions resource - only alloweing new, create and destroy methods
+	resources :sessions, :only => [:new, :create, :destroy]
+	match '/signup',  :to => 'users#new'
+	match '/signin',  :to => 'sessions#new'
+	match '/signout', :to => 'sessions#destroy', :via => :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
